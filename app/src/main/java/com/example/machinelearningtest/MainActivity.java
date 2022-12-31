ff6b44672336892b05555ef9e2ee91358faa2a2d
Subhajit Paul <paulsubhajit78242548@gmail.com>
2022-12-31 23:49:46 +0530

    Added Activity Image Helper. Added Two Buttons in it. One for selecting an image from file another from camera. Added Imageview field for image input and Textview field for OutPut. Classification to be implemented

--- app/src/main/java/com/example/machinelearningtest/MainActivity.java
+++ app/src/main/java/com/example/machinelearningtest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.machinelearningtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.machinelearningtest.helpers.ImageHelperActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onImageButtonClick(View view){
+        Intent intent = new Intent(this, ImageHelperActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/machinelearningtest/helpers/ImageHelperActivity.java
@@ -0,0 +1,121 @@
+package com.example.machinelearningtest.helpers;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.machinelearningtest.R;
+//import com.google.android.gms.tasks.OnFailureListener;
+//import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.mlkit.vision.common.InputImage;
+//import com.google.mlkit.vision.label.ImageLabel;
+//import com.google.mlkit.vision.label.ImageLabeler;
+//import com.google.mlkit.vision.label.ImageLabeling;
+//import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ImageHelperActivity extends AppCompatActivity {
+    private ImageView imageViewInput;
+    private TextView  textViewOutput;
+    private final int r_CODE = 1000;
+//    private ImageLabeler imageLabeler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_helper);
+        textViewOutput = findViewById(R.id.textViewOutput);
+        imageViewInput = findViewById(R.id.imageViewInput);
+
+//        imageLabeler = ImageLabeling.getClient(new ImageLabelerOptions.Builder().setConfidenceThreshold(.9f).build());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+        public void onPickImage(View view){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, r_CODE);
+    }
+
+    public void onStartCamera(View view){
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == r_CODE){
+                assert data != null;
+                Uri uri = data.getData();
+                Bitmap bitmap = loadFromURI(uri);
+                imageViewInput.setImageBitmap(bitmap);
+//                IMGClassifier(bitmap);
+            }
+        }
+    }
+
+    private Bitmap loadFromURI(Uri uri){
+        Bitmap bitmap = null;
+        try {
+            ImageDecoder.Source source;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+                source = ImageDecoder.createSource(getContentResolver(), uri);
+                bitmap = ImageDecoder.decodeBitmap(source);
+            }
+            else{
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+//    private void IMGClassifier(Bitmap bitmap){
+//        InputImage inputImage = InputImage.fromBitmap(bitmap, 0);
+//        imageLabeler.process(inputImage).addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+//            @Override
+//            public void onSuccess(List<ImageLabel> imageLabels) {
+//                if(imageLabels.size() > 0){
+//                    StringBuilder sb = new StringBuilder();
+//                    for (ImageLabel label: imageLabels){
+//                        sb.append(label.getText()).append(" : ").append(label.getConfidence()).append("\n");
+//                    }
+//                    textViewOutput.setText(sb.toString());
+//                }
+//                else{
+//                    textViewOutput.setText("Couldn't Classify");
+//                }
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@androidx.annotation.NonNull Exception e) {
+//                e.printStackTrace();
+//            }
+//        });
+//    }
+}
